7959c818f3e146f1bb4c3dd3bb6ba3f3eefc474a
ma2tomm <tomoshigesongcun010@gmail.com>
2024-07-18 18:25:31 +0900

    Add files via upload

---
+++ slick.js
@@ -0,0 +1,32 @@
+
+
+// トップページスライドショー
+$('.mainimg-slick').slick({
+	autoplay: true,
+	dots: false,				//丸いページナビボタンを表示
+	arrows: false,		
+    fade: true,    	//左右の矢印
+	autoplaySpeed: 2000,	//切り替えのスピード
+});
+
+$('.sabaki-slick').slick({
+	autoplay: true,
+	dots: false,				//丸いページナビボタンを表示
+	arrows: false,		
+    fade: true,    	//左右の矢印
+	autoplaySpeed: 1000,	//切り替えのスピード
+});
+
+$('.group-slick').slick({
+	autoplay: true,
+	dots: false,				//丸いページナビボタンを表示
+	arrows: false,		
+    fade: true,    	//左右の矢印
+	autoplaySpeed: 2000,	//切り替えのスピード
+});
+
+
+
+
+
+
